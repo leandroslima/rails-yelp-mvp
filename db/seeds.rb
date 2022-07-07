d54e3c047cfe59090838eed8afb0c4401e98e352
leandro lima <leandro.desouzalima1@gmail.com>
2022-07-07 10:48:37 -0300

    back complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category = %w(chinese italian japanese french belgian)
+Rating = (0..5).to_a
+
+5.times do
+  restaurant_example = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Category.sample
+  )
+
+  5.times do
+    review = Review.new(
+      rating: Rating.sample,
+      content: Faker::Restaurant.review
+    )
+    review.restaurant = restaurant_example
+    review.save
+  end
+end
